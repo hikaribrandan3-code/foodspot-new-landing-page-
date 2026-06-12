b648a43909e640c4fe124ff852bb0eab7bbe907e
Daiske Brandan <daiskebrandan@mac.home>
2026-06-12 16:08:46 -0300

    Adjust phone cycle to 1.3s

--- src/components/MainSections.tsx
+++ src/components/MainSections.tsx
@@ -234,7 +234,7 @@ function AnimatedPhoneShowcase() {
   useEffect(() => {
     const interval = setInterval(() => {
       setScreenIndex((prev) => (prev + 1) % screens.length);
-    }, 800);
+    }, 1300);
     return () => clearInterval(interval);
   }, []);
 
